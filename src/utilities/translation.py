3b7260f7f3a7456fd341bf2e99d81de6ba299f03
Fingon <70348701+l-Fingon-l@users.noreply.github.com>
2022-12-07 12:10:34 -0100

    Update translation scripts to Teve 4.18.4

--- src/utilities/translation.py
+++ src/utilities/translation.py
@@ -17,7 +17,7 @@ def encrypt(string):
 
     return result
 
-def decrypt(string):
+def decrypt(string, key: str):
     result = ''
     data = string
     while len(data) > 0:
@@ -26,16 +26,18 @@ def decrypt(string):
         if n == 99:
             result += '\n'
         elif n != 0:
-            result += "0)M3}$lF%*(/|WdjviC{5u2Lm4nTb+\n><XZ I]t~h=?-_8DUcAy9oQGErV,ax1[YP:qJB\"p!fSNe6wksH'@#gK7.O&zR;\\ "[n - 1: n]
+            result += key[n - 1: n]
+            #"{#@.}B\nKNwcE*Jvp2/,[:VzOsDaR3%mC_ktI)8oy<-FHeYx6SMbl(P\\=>AQ9X&$Lh4 ?]rgq7G~'!|f+jd5u1UinT0\";ZW"
+            #result += "0)M3}$lF%*(/|WdjviC{5u2Lm4nTb+\n><XZ I]t~h=?-_8DUcAy9oQGErV,ax1[YP:qJB\"p!fSNe6wksH'@#gK7.O&zR;\\ "[n - 1: n]
     return result
 
-while True:
-    encrypted = input('encrypted: ')
-    print('decrypted: "' + decrypt(encrypted) + '"')
+# while True:
+#     encrypted = input('encrypted: ')
+#     print('decrypted: "' + decrypt(encrypted, "{#@.}B\nKNwcE*Jvp2/,[:VzOsDaR3%mC_ktI)8oy<-FHeYx6SMbl(P\\=>AQ9X&$Lh4 ?]rgq7G~'!|f+jd5u1UinT0\";ZW") + '"')
 
 
 def decrypt_re(string):
-    result = '"'
+    result = 'Iji("'
     data = string.group(1)
     while len(data) > 0:
         n = int(string.group(1)[len(data) - 2: len(data)])
@@ -43,11 +45,16 @@ def decrypt_re(string):
         if n == 99:
             result += '\n'
         elif n != 0:
-            result += "0)M3}$lF%*(/|WdjviC{5u2Lm4nTb+\n><XZ I]t~h=?-_8DUcAy9oQGErV," \
-                      "ax1[YP:qJB\"p!fSNe6wksH'@#gK7.O&zR;\\ "[n - 1: n]
+            result += "{#@.}B\nKNwcE*Jvp2/,[:VzOsDaR3%mC_ktI)8oy<-FHeYx6SMbl(P\\=>AQ9X&$Lh4 ?]rgq7G~'!|f+jd5u1UinT0\";ZW"[n - 1: n]
+    decoded.write(result[5:] + '\n')
     return result + '"'
 
-original = Path('C:\\Users\\I\\Desktop\\war3map_refactored.j').read_text()
-p = re.compile(r'Pws\("((?:\d{2})+)"\)')
+decoded = open('decoded.txt', 'w')
+
+original = Path('original.j').read_text()
+#p = re.compile(r'Pws\("((?:\d{2})+)"\)')
+p = re.compile(r'Iji\("((?:\d{2})+)"')
 res = p.sub(decrypt_re, original)
-refactored = Path('C:\\Users\\I\\Desktop\\war3map_refactored_.j').write_text(res)
+refactored = Path('war3map_refactored.j').write_text(res)
+
+decoded.close()
